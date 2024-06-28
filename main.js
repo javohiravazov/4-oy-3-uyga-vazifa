c4a142a1a083a86cae0399091cca505840916568
javohiravazov <skromnie19@gmail.com>
2024-06-28 06:21:41 +0300

    commit-exercise added

--- main.js
+++ main.js
@@ -1,2 +1,157 @@
-let name = A;
-let name = B;
+/* 
+Boolean1
+let A = Number(prompt("Sonni kiriting"));
+if (A > 0) {
+  prompt("berilgan son musbat");
+} else if (A <= 0) {
+  prompt("berilgan son manfiy");
+}
+*/
+/*
+Boolean2
+let A = Number(prompt("Sonni kiriting"));
+if (A % 2 == 0) {
+  prompt("Berilgan son juft soni");
+} else prompt("Berilgan son toq soni");
+
+}
+*/
+/*
+Boolean3
+let A = Number(prompt("Sonni kiriting"));
+if (A % 2 == 0) {
+  prompt("Berilgan son juft soni");
+} else prompt("Berilgan son toq soni");
+
+*/
+/*
+Boolean4
+let A = Number(prompt("Sonni kiriting"));
+let B = Number(prompt("Sonni kiriting"));
+if (A > 2 && B <= 3) {
+  prompt("Javob tog'ri");
+}
+*/
+/*
+Boolean5
+let A = Number(prompt("Sonni kiriting"));
+let B = Number(prompt("Sonni kiriting"));
+if (A >= 0 && B < -2) {
+  prompt("Javob tog'ri");
+}
+*/
+/*
+Boolean6
+let A = Number(prompt("Sonni kiriting"));
+let B = Number(prompt("Sonni kiriting"));
+let C = Number(prompt("Sonni kiriting"));
+if (A <= B <= C) {
+  prompt("Javob tog'ri");
+} else {
+  alert("Natogri qiymat kiritdingiz");
+}
+*/
+/*
+Boolean7
+let A = Number(prompt("Sonni kiriting"));
+let B = Number(prompt("Sonni kiriting"));
+let C = Number(prompt("Sonni kiriting"));
+if (A > B < C) {
+  prompt("Javob tog'ri");
+} else {
+  alert("Natogri qiymat kiritdingiz");
+}
+*/
+/*
+Boolean8
+let A = Number(prompt("A Sonni kiriting"));
+let B = Number(prompt("B sonni kiriting"));
+if (A % 2 == 0 && B % 2 == 0) {
+  prompt("Javob tog'ri");
+  if (A % 2 == 0 && B % 2 == 0) {
+  prompt("Berilgan sonlar juft soni");
+} else prompt("Berilgan sonlar toq soni");
+}
+*/
+/*
+Boolean9
+let A = Number(prompt("A Sonni kiriting"));
+let B = Number(prompt("B sonni kiriting"));
+if (A % 2 == 0 && B % 2 == 0) {
+  prompt("Javob tog'ri");if (B % 2 == 0) {
+  prompt("Berilgan sonlar juft soni");
+} else prompt("Berilgan sonlar toq soni");
+}
+*/
+/*
+Boolean10
+let A = Number(prompt("A Sonni kiriting"));
+let B = Number(prompt("B sonni kiriting"));
+if (A % 2 == 0 && B % 2 == 0) {
+  prompt("Javob tog'ri");
+}
+*/
+/*
+Boolean11
+let A = Number(prompt("A Sonni kiriting"));
+let B = Number(prompt("B Sonni kiriting"));
+if (A % 2 == 0 && B % 2 == 0) {
+  prompt("Berilgan sonlar juft soni");
+} else prompt("Berilgan sonlar toq soni");
+*/
+
+/*
+Boolean12
+let A = Number(prompt("Sonni kiriting"));
+let B = Number(prompt("Sonni kiriting"));
+let C = Number(prompt("Sonni kiriting"));
+if (A > 0 && B > 0 && C > 0) {
+  prompt("Berilgan sonlar musbat soni");
+} else prompt("Berilgan sonlarda orasida manfiy son qatnashgan");
+*/
+
+/*
+Boolean13
+let A = Number(prompt("Sonni kiriting"));
+let B = Number(prompt("Sonni kiriting"));
+let C = Number(prompt("Sonni kiriting"));
+if (A > 0 && B < 0 && C < 0) {
+  prompt("Berilgan sonlar orasida musbat soni qatnashgan");
+} else prompt("Berilgan sonlarda orasida manfiy son qatnashgan");
+if (A < 0 && B > 0 && C < 0) {
+  prompt("Berilgan sonlar orasida musbat soni qatnashgan");
+} else prompt("Berilgan sonlarda orasida manfiy son qatnashgan");
+if (A < 0 && B < 0 && C > 0) {
+  prompt("Berilgan sonlar orasida musbat soni qatnashgan");
+} else prompt("Berilgan sonlarda orasida manfiy son qatnashgan");
+*/
+/*
+Boolean14
+let A = Number(prompt("Sonni kiriting"));
+let B = Number(prompt("Sonni kiriting"));
+let C = Number(prompt("Sonni kiriting"));
+if (A > 0 && B < 0 && C < 0) {
+  prompt("Berilgan sonlar orasida musbat soni qatnashgan");
+} else prompt("Berilgan sonlarda orasida manfiy son qatnashgan");
+if (A < 0 && B > 0 && C < 0) {
+  prompt("Berilgan sonlar orasida musbat soni qatnashgan");
+} else prompt("Berilgan sonlarda orasida manfiy son qatnashgan");
+if (A < 0 && B < 0 && C > 0) {
+  prompt("Berilgan sonlar orasida musbat soni qatnashgan");
+} else prompt("Berilgan sonlarda orasida manfiy son qatnashgan");
+*/
+/*
+Boolean15
+let A = Number(prompt("Sonni kiriting"));
+let B = Number(prompt("Sonni kiriting"));
+let C = Number(prompt("Sonni kiriting"));
+if (A > 0 && B > 0 && C < 0) {
+  prompt("Berilgan sonlar orasida 2 ta musbat son");
+} else prompt("Berilgan sonlar orasida 2 ta musbat son");
+if (A < 0 && B > 0 && C > 0) {
+  prompt("Berilgan sonlar orasida musbat soni qatnashgan");
+} else prompt("Berilgan sonlar orasida 2 ta musbat son");
+if (A < 0 && B < 0 && C > 0) {
+  prompt("Berilgan sonlar orasida 2 ta musbat son");
+} 
+*/
